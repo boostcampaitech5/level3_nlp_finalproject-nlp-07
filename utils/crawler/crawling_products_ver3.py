c7ea7b5a8492ff4dd9ccfb71c306ffecaf7b3d4e
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-07-07 15:25:29 +0900

    [Fix] 품목 크롤링 ver3 리스트 형식 변화에 따라 코드 변경
    #5

--- utils/crawler/crawling_products_ver3.py
+++ utils/crawler/crawling_products_ver3.py
@@ -6,7 +6,7 @@ from pytz import timezone
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from data.search_list import search_products_list
-
+from tqdm import tqdm
 
 def crawling_products(search_list):
     '''
@@ -17,106 +17,110 @@ def crawling_products(search_list):
     data = []
 
     name_list = []
+    total_size = sum(len(v) for v in search_list.values())
 
-    for search_name in search_list:
+    for search_dict in tqdm(search_list, total=total_size):
+        for search_name in search_list[search_dict]:
+            print(search_name)
 
-        options = Options()
-        options.add_argument("--window-size=300,100")  # 원하는 창 크기를 지정할 수 있습니다.
 
-        driver = webdriver.Chrome(executable_path='chromedriver', options=options)
+            options = Options()
+            options.add_argument("--window-size=300,100")  # 원하는 창 크기를 지정할 수 있습니다.
 
-        url = f'https://www.coupang.com/np/search?component=&q={search_name}&channel=user'
-        driver.get(url)
+            driver = webdriver.Chrome(executable_path='chromedriver', options=options)
 
-        lis = driver.find_elements(By.TAG_NAME, 'li')
+            url = f'https://www.coupang.com/np/search?component=&q={search_name}&channel=user'
+            driver.get(url)
 
-        cnt = 0
+            lis = driver.find_elements(By.TAG_NAME, 'li')
 
-        for li in lis:
-            try:
+            cnt = 0
 
-                # 제품명
-                name = li.find_element(By.CLASS_NAME, 'name').text
-                name = name.replace('\"', '')  # 쉼표 제거
-                name = search_name + ' ' + name
+            for li in lis:
+                try:
 
-                if name in name_list:
-                    continue
-                name_list.append(name)
+                    # 제품명
+                    name = li.find_element(By.CLASS_NAME, 'name').text
+                    name = name.replace('\"', '')  # 쉼표 제거
+                    name = search_name + ' ' + name
 
-                # 제품 고유번호
-                # unique_product_id = li.find_element(By.CLASS_NAME, 'search-product').get_attribute('data-product-id')
+                    if name in name_list:
+                        continue
+                    name_list.append(name)
 
-                # 가격
-                price = li.find_element(By.CLASS_NAME, 'price-value').text
-                price = price.replace(',', '')  # 쉼표 제거
-                price = price.replace('\"', '')  # 쌍따옴표 제거
+                    # 제품 고유번호
+                    # unique_product_id = li.find_element(By.CLASS_NAME, 'search-product').get_attribute('data-product-id')
 
-                # 리뷰수
-                review_cnt = li.find_element(By.CLASS_NAME, 'rating-total-count').text
-                review_cnt = review_cnt.replace('(', '').replace(')', '')  # 괄호 제거
+                    # 가격
+                    price = li.find_element(By.CLASS_NAME, 'price-value').text
+                    price = price.replace(',', '')  # 쉼표 제거
+                    price = price.replace('\"', '')  # 쌍따옴표 제거
 
-                # 리뷰수가 빈 문자열이 아니면 int로 변환
-                if review_cnt != '':
-                    review_cnt = int(review_cnt)
-                else:
-                    review_cnt = 0  # 또는 다른 기본값을 지정할 수 있습니다.
+                    # 리뷰수
+                    review_cnt = li.find_element(By.CLASS_NAME, 'rating-total-count').text
+                    review_cnt = review_cnt.replace('(', '').replace(')', '')  # 괄호 제거
 
-                # 리뷰 평균 별점
-                rating = li.find_element(By.CLASS_NAME, 'rating').text
-                if rating != '':
-                    rating = float(rating)
-                else:
-                    rating = 0.0  # 또는 다른 기본값을 지정할 수 있습니다.
+                    # 리뷰수가 빈 문자열이 아니면 int로 변환
+                    if review_cnt != '':
+                        review_cnt = int(review_cnt)
+                    else:
+                        review_cnt = 0  # 또는 다른 기본값을 지정할 수 있습니다.
 
-                # 광고 여부
-                ad_elements = li.find_elements(By.CLASS_NAME, 'ad-badge-text')
+                    # 리뷰 평균 별점
+                    rating = li.find_element(By.CLASS_NAME, 'rating').text
+                    if rating != '':
+                        rating = float(rating)
+                    else:
+                        rating = 0.0  # 또는 다른 기본값을 지정할 수 있습니다.
 
-                # 제품 상세 페이지 URL
-                url = li.find_element(By.CLASS_NAME, 'search-product-link').get_attribute('href')
+                    # 광고 여부
+                    ad_elements = li.find_elements(By.CLASS_NAME, 'ad-badge-text')
 
-                # TODO 재크롤링 필요한 정보들 (브랜드명, 브랜드점수)
-                # driver.get(url)
+                    # 제품 상세 페이지 URL
+                    url = li.find_element(By.CLASS_NAME, 'search-product-link').get_attribute('href')
 
-                # 브랜드명
-                # brand_name = driver.find_element(By.CLASS_NAME, 'prod-sale-vendor-name').text
+                    # TODO 재크롤링 필요한 정보들 (브랜드명, 브랜드점수)
+                    # driver.get(url)
 
-                # 브랜드점수
-                # brand_rating = driver.find_element(By.CLASS_NAME, 'seller-rating with-company').text
+                    # 브랜드명
+                    # brand_name = driver.find_element(By.CLASS_NAME, 'prod-sale-vendor-name').text
 
-                if len(ad_elements) > 0:
-                    ad_yn = 'Y'
-                    # 광고 제품은 제외
-                    continue
-                else:
-                    ad_yn = 'N'
+                    # 브랜드점수
+                    # brand_rating = driver.find_element(By.CLASS_NAME, 'seller-rating with-company').text
 
-                if review_cnt == '':
-                    continue
-                if rating == '':
-                    continue
-                if price == '':
-                    continue
-                if name == '':
-                    continue
+                    if len(ad_elements) > 0:
+                        ad_yn = 'Y'
+                        # 광고 제품은 제외
+                        continue
+                    else:
+                        ad_yn = 'N'
 
-                # # 리뷰 50개 이하는 제외
-                if review_cnt <= 50:
-                    continue
+                    if review_cnt == '':
+                        continue
+                    if rating == '':
+                        continue
+                    if price == '':
+                        continue
+                    if name == '':
+                        continue
 
-                if rating <= 3.0:
-                    continue
+                    # # 리뷰 50개 이하는 제외
+                    if review_cnt <= 50:
+                        continue
 
-                data.append([search_name, name, price, review_cnt, rating, ad_yn, url])
-                cnt += 1
+                    if rating <= 3.0:
+                        continue
 
-                if cnt == 10:
-                    break
+                    data.append([search_name, name, price, review_cnt, rating, ad_yn, url])
+                    cnt += 1
 
-            except Exception as e:
-                pass
+                    if cnt == 10:
+                        break
 
-    driver.quit()
+                except Exception as e:
+                    pass
+
+            driver.quit()
 
     # Convert list of lists into dataframe
     df_output = pd.DataFrame(data, columns=['search_name', 'name', 'price', 'review_cnt', 'rating', 'ad_yn', 'URL'])
@@ -124,12 +128,12 @@ def crawling_products(search_list):
     # time
     KST = datetime.datetime.now(timezone('Asia/Seoul')).strftime("%Y%m%d_%H%M%S")
     # Write dataframe to CSV
-    df_output.to_csv(f"product_{KST}_{search_name}.csv", index=False)
+    df_output.to_csv(f"product_list_{KST}.csv", index=False)
 
 
 if __name__ == '__main__':
-
     # 검색할 키워드 가져오기
     search_list = search_products_list()
+    print(search_list)
 
     crawling_products(search_list)
