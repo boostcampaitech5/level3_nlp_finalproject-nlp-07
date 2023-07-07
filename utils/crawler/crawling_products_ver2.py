cf371b25296fd0285e98f11413c56ebbd18245db
최윤진 <89494907+yunjinchoidev@users.noreply.github.com>
2023-07-07 10:05:54 +0900

    [Fix] 크롤러 ver2 함수화
    #5

--- utils/crawler/crawling_products_ver2.py
+++ utils/crawler/crawling_products_ver2.py
@@ -1,24 +1,20 @@
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import re
-import time
-# Extract product codes
-product_codes = ['치킨']
-outputname='치킨'
+import datetime
+from pytz import timezone
 
-driver = webdriver.Chrome(executable_path='chromedriver')
 
-data = []
-name_list = []
+def product_one_crawler(search_name):
+    driver = webdriver.Chrome(executable_path='chromedriver')
 
-for product_code in product_codes:
+    data = []
+    name_list = [] # 중복 체크용
 
-    url = 'https://www.coupang.com/np/search?component=&q=치킨&channel=user'
+    url = f'https://www.coupang.com/np/search?component=&q={search_name}&channel=user'
     driver.get(url)
 
     lis = driver.find_elements(By.TAG_NAME, 'li')
-
 
     cnt = 0
 
@@ -29,6 +25,7 @@ for product_code in product_codes:
             name = li.find_element(By.CLASS_NAME, 'name').text
             name = name.replace('\"', '')  # 쉼표 제거
 
+            # 중복 제거
             if name in name_list:
                 continue
             name_list.append(name)
@@ -41,23 +38,30 @@ for product_code in product_codes:
             price = price.replace(',', '')  # 쉼표 제거
             price = price.replace('\"', '')  # 쌍따옴표 제거
 
-
             # 리뷰수
             review_cnt = li.find_element(By.CLASS_NAME, 'rating-total-count').text
             review_cnt = review_cnt.replace('(', '').replace(')', '')  # 괄호 제거
-            review_cnt = int(review_cnt)
+
+            # 리뷰수가 빈 문자열이 아니면 int로 변환
+            if review_cnt != '':
+                review_cnt = int(review_cnt)
+            else:
+                review_cnt = 0  # 또는 다른 기본값을 지정할 수 있습니다.
 
             # 리뷰 평균 별점
             rating = li.find_element(By.CLASS_NAME, 'rating').text
-            # int
-            rating = float(rating)
+
+            if rating != '':
+                rating = float(rating)
+            else:
+                rating = 0.0  # 또는 다른 기본값을 지정할 수 있습니다.
+
 
             # 광고 여부
             ad_elements = li.find_elements(By.CLASS_NAME, 'ad-badge-text')
 
             # 제품 상세 페이지 URL
             url = li.find_element(By.CLASS_NAME, 'search-product-link').get_attribute('href')
-
 
             # TODO 재크롤링 필요한 정보들 (브랜드명, 브랜드점수)
             # driver.get(url)
@@ -91,7 +95,6 @@ for product_code in product_codes:
             if rating <= 3.0:
                 continue
 
-
             data.append([name, price, review_cnt, rating, ad_yn, url])
             cnt += 1
 
@@ -101,10 +104,17 @@ for product_code in product_codes:
         except Exception as e:
             pass
 
-driver.quit()
+    driver.quit()
 
-# Convert list of lists into dataframe
-df_output = pd.DataFrame(data, columns=['name','price', 'review_cnt', 'rating', 'ad_yn', 'URL'])
+    # Convert list of lists into dataframe
+    df_output = pd.DataFrame(data, columns=['name', 'price', 'review_cnt', 'rating', 'ad_yn', 'URL'])
 
-# Write dataframe to CSV
-df_output.to_csv(f"product_{outputname}_ver2.csv", index=False)
+    # time
+    KST = datetime.datetime.now(timezone('Asia/Seoul')).strftime("%Y%m%d_%H%M%S")
+    # Write dataframe to CSV
+    df_output.to_csv(f"product_{KST}_{search_name}.csv", index=False)
+
+
+if __name__ == '__main__':
+    search_name = '떡볶이'
+    product_one_crawler(search_name)
