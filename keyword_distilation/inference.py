c6f948349c8ad4194fca5a57586359a7d7a0f37d
yunjinchoidev <yunjinchoidev@gmail.com>
2023-07-11 06:21:03 +0000

    [Fix] topk 지표 추가, MRR 지표 추가
    #8

--- keyword_distilation/inference.py
+++ keyword_distilation/inference.py
@@ -1,48 +1,13 @@
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
-from operator import itemgetter
-import pandas as pd
-import sqlite3
-from keybert import KeyBERT
 import pandas as pd
 import os
 import pymysql
 import configparser
-from sklearn.metrics import precision_score, recall_score, f1_score, average_precision_score, ndcg_score
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 from operator import itemgetter
-import pandas as pd
-import pymysql
-import configparser
 from keybert import KeyBERT
 import numpy as np
-
-def compute_precision(y_true, y_pred):
-    return precision_score(y_true, y_pred)
-
-def compute_recall(y_true, y_pred):
-    return recall_score(y_true, y_pred)
-
-def compute_f1_score(y_true, y_pred):
-    return f1_score(y_true, y_pred)
-
-def compute_map_score(y_true, y_pred):
-    return average_precision_score(y_true, y_pred)
-
-def compute_ndcg_score(true_scores, predicted_scores):
-    return ndcg_score(true_scores, predicted_scores)
-
-def compute_mrr_score(y_true, y_pred):
-    scores = 0
-    for t, p in zip(y_true, y_pred):
-        if p in t:
-            scores += 1 / (t.index(p) + 1)
-    return scores / len(y_true)
-
-
-
+import matplotlib.pyplot as plt
 
 # 환경 설정
 env = os.getenv('MY_APP_ENV', 'local')  # 기본값은 'local'
@@ -63,14 +28,11 @@ evaluations = pd.read_sql(query, conn)
 # 키워드 추출 모델
 kw_model = KeyBERT('distilbert-base-nli-mean-tokens')
 
-
 precision_scores = []
-recall_scores = []
-f1_scores = []
-map_scores = []
-mrr_scores = []
+top3_hits = 0
 
 for _, evaluation in evaluations.iterrows():
+    
     user_query = evaluation['query']
     search_name = evaluation['search_name']
     correct_product_id = int(evaluation['product_id'])
@@ -94,32 +56,30 @@ for _, evaluation in evaluations.iterrows():
     product_similarities = list(zip(products['product_id'], query_similarities))
     product_similarities.sort(key=itemgetter(1), reverse=True)
 
-    # 추천된 상품 ID 리스트
-    recommended_product_ids = [id for id, _ in product_similarities]
-
     # 평가 점수 계산
-    y_true = [1 if id == correct_product_id else 0 for id, _ in product_similarities[:10]]
-    y_pred = [1] * len(y_true)
-    precision_scores.append(compute_precision(y_true, y_pred))
-    recall_scores.append(compute_recall(y_true, y_pred))
-    f1_scores.append(compute_f1_score(y_true, y_pred))
-    map_scores.append(compute_map_score(y_true, y_pred))
+    y_true = [1 if id == correct_product_id else 0 for id, _ in product_similarities[:3]]  # Top 5
+    precision_scores.append(sum(y_true) / len(y_true))  # Precision@5
 
-    # MRR 계산
-    if correct_product_id in recommended_product_ids:
-        rank = recommended_product_ids.index(correct_product_id) + 1
-        mrr_scores.append(1 / rank)
+    # Top 3 hit count
+    top3_products = [id for id, _ in product_similarities[:3]]  # Top 3
+    if correct_product_id in top3_products:
+        # prod name 을 가져와야함
 
+
+        top3_hits += 1
 
 # 전체 평가 점수의 평균 계산
 mean_precision = np.mean(precision_scores)
-mean_recall = np.mean(recall_scores)
-mean_f1 = np.mean(f1_scores)
-mean_map = np.mean(map_scores)
-mean_mrr = np.mean(mrr_scores)
+top3_hit_rate = top3_hits / len(evaluations)
 
-print(f"Mean Precision: {mean_precision}")
-print(f"Mean Recall: {mean_recall}")
-print(f"Mean F1 Score: {mean_f1}")
-print(f"Mean MAP: {mean_map}")
-print(f"Mean MRR: {mean_mrr}")
+print(f"Mean Precision@3: {mean_precision}")
+print(f"Top 3 Hit Rate: {top3_hit_rate}")
+
+# Precision@5 scores chart
+plt.figure(figsize=(10, 5))
+plt.plot(precision_scores, marker='o')
+plt.title('Precision@3 scores for each query')
+plt.xlabel('Query Index')
+plt.ylabel('Precision@3')
+plt.grid(True)
+plt.savefig('precision_at_3.png')
