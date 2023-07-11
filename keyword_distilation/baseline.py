d3cba0ea69ad2c5a681aea89051fe6dbd4f95a4a
yunjinchoidev <yunjinchoidev@gmail.com>
2023-07-11 04:05:04 +0000

    [Fix] db 에서 데이터 가져와서 키워드 추출하고 저장하는 방식으로 코드 수정
    #8

--- keyword_distilation/baseline.py
+++ keyword_distilation/baseline.py
@@ -1,55 +1,117 @@
+# from keybert import KeyBERT
+# from transformers import AutoTokenizer, AutoModel
+# from sklearn.cluster import KMeans
+# import pandas as pd
+# import numpy as np
+# from keybert import KeyBERT
+# from kiwipiepy import Kiwi
+# from transformers import BertModel
+# import pandas as pd
+# import os
+# import pandas as pd
+# import pymysql
+# import scipy.io
+# import csv
+# import pymysql
+# import os
+# import configparser
+# from tqdm import tqdm
+
+
+
+# if __name__ == "__main__":
+
+#     # 환경 설정
+#     env = os.getenv('MY_APP_ENV', 'local')  # 기본값은 'local'
+
+#     def create_conn():
+#         config = configparser.ConfigParser()
+#         config.read(f'../config/config-{env}.ini')  # 환경에 맞는 설정 파일 읽기
+#         mysql_config = config['mysql']
+#         return pymysql.connect(host=mysql_config['host'], user=mysql_config['user'], password=mysql_config['password'],
+#                             db=mysql_config['db'], charset=mysql_config['charset'])
+
+
+#     # MySQL 연결 설정
+#     conn = create_conn()
+
+#     # MySQL query
+#     query = "SELECT * FROM reviews_ver11_15_100"
+
+#     # pandas를 사용하여 query 실행하고 DataFrame으로 결과 저장
+#     reviews = pd.read_sql(query, conn)['context']
+
+#     # 연결 종료
+#     conn.close()
+
+
+
+
+#     keywords = []
+#     # 키워드 추출
+#     kw_model = KeyBERT('distilbert-base-nli-mean-tokens')
+
+#     for review in reviews:    
+#         extracted_keywords = kw_model.extract_keywords(review)
+#         keywords.extend(extracted_keywords)
+
+
+#     print(keywords)
+
+
 from keybert import KeyBERT
-from transformers import AutoTokenizer, AutoModel
-from sklearn.cluster import KMeans
 import pandas as pd
-import numpy as np
-from keybert import KeyBERT
+import os
+import pymysql
+import configparser
 
+# 환경 설정
+env = os.getenv('MY_APP_ENV', 'local')  # 기본값은 'local'
 
-reviews = pd.read_csv('../utils/db_scripts/output.csv')['context'][:10]
+def create_conn():
+    config = configparser.ConfigParser()
+    config.read(f'../config/config-{env}.ini')  # 환경에 맞는 설정 파일 읽기
+    mysql_config = config['mysql']
+    return pymysql.connect(host=mysql_config['host'], user=mysql_config['user'], password=mysql_config['password'],
+                        db=mysql_config['db'], charset=mysql_config['charset'])
 
+# MySQL 연결 설정
+conn = create_conn()
 
+# MySQL query
+query = "SELECT * FROM reviews_ver11_15_100"
 
+# pandas를 사용하여 query 실행하고 DataFrame으로 결과 저장
+reviews = pd.read_sql(query, conn)
+
+# product_id별로 리뷰를 합침
+grouped_reviews = reviews.groupby('prod_id')['context'].apply(' '.join).reset_index()
+
+# 키워드 추출
+kw_model = KeyBERT('distilbert-base-nli-mean-tokens')
+
+# 키워드 저장을 위한 빈 리스트 생성
 keywords = []
-for review in reviews:
-    # 키워드 추출
-    kw_model = KeyBERT()
-    extracted_keywords = kw_model.extract_keywords(review)
-    keywords.extend(extracted_keywords)
 
-tokenizer = AutoTokenizer.from_pretrained('klue/roberta-large')
-model = AutoModel.from_pretrained('klue/roberta-large')
+for review in grouped_reviews['context']:    
+    extracted_keywords = kw_model.extract_keywords(review, keyphrase_ngram_range=(1,1), stop_words='english', top_n=20)
+    keywords.append(','.join([word for word, _ in extracted_keywords]))
 
-vectors = []
+# 키워드를 데이터프레임에 추가
+grouped_reviews['keywords'] = keywords
 
-from keybert import KeyBERT
-from transformers import AutoTokenizer, AutoModel
-from sklearn.cluster import KMeans
-import pandas as pd
-import numpy as np
+# print(grouped_reviews)
 
-tokenizer = AutoTokenizer.from_pretrained('klue/roberta-large')
-model = AutoModel.from_pretrained('klue/roberta-large')
+# MySQL에 업데이트할 쿼리 생성
+update_query = "UPDATE products_ver11_15_100 SET keywords = %s WHERE product_id = %s"
 
-vectors = []
+# 쿼리 실행 및 커밋
+with conn.cursor() as cursor:
+    for i, row in grouped_reviews.iterrows():
+        cursor.execute(update_query, (row['keywords'], row['prod_id']))
+    conn.commit()
 
-# 키워드 벡터화
-for keyword, _ in extracted_keywords:
-    inputs = tokenizer(keyword, return_tensors='pt', truncation=True, padding=True)
-    outputs = model(**inputs)
-    vectors.append(outputs.last_hidden_state.mean(dim=1).detach().numpy().squeeze())
+# 연결 종료
+conn.close()
 
-print(vectors)
-
-# 클러스터링
-vectors = np.stack(vectors)  # Convert list of arrays into a single 2D array
-kmeans = KMeans(n_clusters=2, random_state=0).fit(vectors)
-
-# 클러스터 결과 출력
-for idx, label in enumerate(set(kmeans.labels_)):
-    print(f'Cluster {idx+1}:')
-    print([keywords[i][0] for i in range(len(kmeans.labels_)) if kmeans.labels_[i]==label])
-    print()
-
-
-
+print('Done!')
