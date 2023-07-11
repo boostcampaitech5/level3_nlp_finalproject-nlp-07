b28693908bed2f2a37dde4d106160fc863a38648
yunjinchoidev <yunjinchoidev@gmail.com>
2023-07-11 06:32:54 +0000

    [Refactor] 코드 정리
    #8

--- keyword_distilation/baseline.py
+++ keyword_distilation/baseline.py
@@ -1,64 +1,3 @@
-# from keybert import KeyBERT
-# from transformers import AutoTokenizer, AutoModel
-# from sklearn.cluster import KMeans
-# import pandas as pd
-# import numpy as np
-# from keybert import KeyBERT
-# from kiwipiepy import Kiwi
-# from transformers import BertModel
-# import pandas as pd
-# import os
-# import pandas as pd
-# import pymysql
-# import scipy.io
-# import csv
-# import pymysql
-# import os
-# import configparser
-# from tqdm import tqdm
-
-
-
-# if __name__ == "__main__":
-
-#     # 환경 설정
-#     env = os.getenv('MY_APP_ENV', 'local')  # 기본값은 'local'
-
-#     def create_conn():
-#         config = configparser.ConfigParser()
-#         config.read(f'../config/config-{env}.ini')  # 환경에 맞는 설정 파일 읽기
-#         mysql_config = config['mysql']
-#         return pymysql.connect(host=mysql_config['host'], user=mysql_config['user'], password=mysql_config['password'],
-#                             db=mysql_config['db'], charset=mysql_config['charset'])
-
-
-#     # MySQL 연결 설정
-#     conn = create_conn()
-
-#     # MySQL query
-#     query = "SELECT * FROM reviews_ver11_15_100"
-
-#     # pandas를 사용하여 query 실행하고 DataFrame으로 결과 저장
-#     reviews = pd.read_sql(query, conn)['context']
-
-#     # 연결 종료
-#     conn.close()
-
-
-
-
-#     keywords = []
-#     # 키워드 추출
-#     kw_model = KeyBERT('distilbert-base-nli-mean-tokens')
-
-#     for review in reviews:    
-#         extracted_keywords = kw_model.extract_keywords(review)
-#         keywords.extend(extracted_keywords)
-
-
-#     print(keywords)
-
-
 from keybert import KeyBERT
 import pandas as pd
 import os
@@ -79,7 +18,7 @@ def create_conn():
 conn = create_conn()
 
 # MySQL query
-query = "SELECT * FROM reviews_ver11_15_100"
+query = "SELECT * FROM reviews_ver12"
 
 # pandas를 사용하여 query 실행하고 DataFrame으로 결과 저장
 reviews = pd.read_sql(query, conn)
