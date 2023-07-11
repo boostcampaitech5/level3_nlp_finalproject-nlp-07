c505c72cf126134a608bd2b9a366ed0119b8c45a
yunjinchoidev <yunjinchoidev@gmail.com>
2023-07-11 06:21:25 +0000

    [Fix] topk, MRR 지표 추가
    #8

--- keyword_distilation/inference.py
+++ keyword_distilation/inference.py
@@ -8,6 +8,13 @@ from operator import itemgetter
 from keybert import KeyBERT
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import configparser
+import pandas as pd
+import pymysql
+from keybert import KeyBERT
+import matplotlib.pyplot as plt
+import numpy as np
 
 # 환경 설정
 env = os.getenv('MY_APP_ENV', 'local')  # 기본값은 'local'
@@ -28,27 +35,26 @@ evaluations = pd.read_sql(query, conn)
 # 키워드 추출 모델
 kw_model = KeyBERT('distilbert-base-nli-mean-tokens')
 
-precision_scores = []
-top3_hits = 0
+top_k = 3
+hit_counts = []
+mrr_scores = []
 
 for _, evaluation in evaluations.iterrows():
-    
     user_query = evaluation['query']
     search_name = evaluation['search_name']
     correct_product_id = int(evaluation['product_id'])
 
     # 쿼리 키워드 추출
-    query_keywords = kw_model.extract_keywords(user_query, keyphrase_ngram_range=(1,1), stop_words='english', top_n=20)
-    query_keywords = ','.join([word for word, _ in query_keywords])
-
+    query_keywords = kw_model.extract_keywords(user_query, keyphrase_ngram_range=(1,1), stop_words='english')
+    query_keywords = [word for word, _ in query_keywords]
+    
     # 데이터베이스에서 상품 정보 가져오기
     query = f"SELECT product_id, keywords FROM products_ver12 WHERE search_name = '{search_name}'" 
     products = pd.read_sql(query, conn)
 
     # 쿼리와 각 상품 키워드간의 유사성 계산
-    vectorizer = CountVectorizer().fit_transform([query_keywords] + list(products['keywords']))
+    vectorizer = CountVectorizer().fit_transform([user_query] + list(products['keywords']))
     vectors = vectorizer.toarray()
-
     csim = cosine_similarity(vectors)
     query_similarities = csim[0, 1:]  # 첫 번째 행은 쿼리와 각 상품 사이의 유사성을 나타냅니다.
 
@@ -56,30 +62,29 @@ for _, evaluation in evaluations.iterrows():
     product_similarities = list(zip(products['product_id'], query_similarities))
     product_similarities.sort(key=itemgetter(1), reverse=True)
 
-    # 평가 점수 계산
-    y_true = [1 if id == correct_product_id else 0 for id, _ in product_similarities[:3]]  # Top 5
-    precision_scores.append(sum(y_true) / len(y_true))  # Precision@5
+    # 추천된 상품 ID 리스트
+    recommended_product_ids = [id for id, _ in product_similarities]
 
-    # Top 3 hit count
-    top3_products = [id for id, _ in product_similarities[:3]]  # Top 3
-    if correct_product_id in top3_products:
-        # prod name 을 가져와야함
+    # Top-k Hit 확인
+    if correct_product_id in recommended_product_ids[:top_k]:
+        hit_counts.append(1)
+        
+        # MRR 계산
+        rank = recommended_product_ids.index(correct_product_id) + 1
+        mrr_scores.append(1 / rank)
+    else:
+        hit_counts.append(0)
 
+# 평가 점수 계산
+top_k_hit_rate = sum(hit_counts) / len(hit_counts)
+mean_mrr = sum(mrr_scores) / len(mrr_scores)
 
-        top3_hits += 1
+print(f"Top-{top_k} Hit Rate: {top_k_hit_rate}")
+print(f"Mean MRR: {mean_mrr}")
 
-# 전체 평가 점수의 평균 계산
-mean_precision = np.mean(precision_scores)
-top3_hit_rate = top3_hits / len(evaluations)
-
-print(f"Mean Precision@3: {mean_precision}")
-print(f"Top 3 Hit Rate: {top3_hit_rate}")
-
-# Precision@5 scores chart
-plt.figure(figsize=(10, 5))
-plt.plot(precision_scores, marker='o')
-plt.title('Precision@3 scores for each query')
-plt.xlabel('Query Index')
-plt.ylabel('Precision@3')
-plt.grid(True)
-plt.savefig('precision_at_3.png')
+# 결과 시각화
+plt.figure(figsize=(12, 6))
+plt.bar(['Top-k Hit Rate', 'Mean MRR'], [top_k_hit_rate, mean_mrr])
+plt.ylabel('Score')
+plt.title('Evaluation Results')
+plt.savefig('evaluation_results.png')
