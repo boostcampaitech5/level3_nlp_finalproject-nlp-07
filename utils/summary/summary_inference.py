e20c18c216e2ae9d9dd13c8b0603635838a118ec
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2023-07-13 05:33:52 +0000

    [Fix] 특수 문자, 문장 부호, 조건 태그 제거 후 평가로 수정

--- utils/summary/summary_inference.py
+++ utils/summary/summary_inference.py
@@ -11,20 +11,30 @@ from datetime import datetime
 from pytz import timezone
 
 
-# 특수문자, 초성어 제거
 def clean_text(sent):
-    sent_clean = re.sub("[^가-힣ㄱ-ㅎㅏ-ㅣ\\s0-9a-zA-Z]", " ", sent)
-    sent_clean = re.sub("[ㄱ-ㅎㅏ-ㅣ]+", "", sent_clean)
-    sent_clean = " ".join(sent_clean.split())
-    sent_clean = sent_clean.strip()
-    return sent_clean
+    """
+    특수 문자, 문장 부호, 조건 태그 제거
+    """
+    sent = re.sub("<[^>]+>", "", sent)  # 조건 태그 제거
+    sent = re.sub("[^가-힣ㄱ-ㅎㅏ-ㅣ\\s0-9a-zA-Z]", " ", sent)
+    sent = re.sub("[ㄱ-ㅎㅏ-ㅣ]+", "", sent)
+    sent = " ".join(sent.split())
+    sent = sent.strip()
+    return sent
+
 
 # 입력 프롬프트 제작
 def build_text(keyword, review):
-    if args.format == "my":
+    if args.format == "my-v0":
         text = (  # multi-line string
             f"### 명령어: 다음의 {keyword}에 대한 리뷰를 특성 세부설명 형식으로 요약하세요\n\n"
             "### 리뷰: " + review.strip() + "\n\n"
+            "### 요약:"
+        )
+    elif args.format == "my-v1":
+        text = (
+            f"### 명령어: 다음 상품에 대한 리뷰에서 상품의 특성을 요약하세요\n\n"
+            "### 리뷰: " + review + "\n\n"
             "### 요약:"
         )
     elif args.format == "koalpaca":
@@ -35,8 +45,10 @@ def build_text(keyword, review):
         )
     elif args.format == "kullm":
         instruction = f"다음의 {keyword}에 대한 리뷰를 특성 세부설명 형식으로 요약하세요."
-        
-        text = "아래는 작업을 설명하는 명령어와 추가 컨텍스트를 제공하는 입력이 짝을 이루는 예제입니다. 요청을 적절히 완료하는 응답을 작성하세요.\n\n### 명령어:\n{instruction}\n\n### 입력:\n{input}\n\n### 응답:\n".format(instruction=instruction, input=review)
+
+        text = "아래는 작업을 설명하는 명령어와 추가 컨텍스트를 제공하는 입력이 짝을 이루는 예제입니다. 요청을 적절히 완료하는 응답을 작성하세요.\n\n### 명령어:\n{instruction}\n\n### 입력:\n{input}\n\n### 응답:\n".format(
+            instruction=instruction, input=review
+        )
 
     return text
 
@@ -63,36 +75,38 @@ def inference(model, tokenizer, test_data: pd.DataFrame):
 
     result_list = []
     preds = []
-    for item in tqdm(test_data.itertuples(), desc="Making predictions", total=len(test_data)):
+    for item in tqdm(
+        test_data.itertuples(), desc="Making predictions", total=len(test_data)
+    ):
         clean_review = clean_text(item.input)
-        input_text = build_text(item.search_name, item.input)
+        input_text = build_text(item.search_name, clean_review)
         generated_text = ask(input_text)
-        generated_text = clean_text(generated_text)
         result_item = {
             "id": item.id,
             "input": clean_review,
             "output": generated_text,
             "score": None,
         }
-        preds.append(generated_text)
+        preds.append(clean_text(generated_text))
         result_list.append(result_item)
 
     total_score = rouge.compute(
         predictions=preds,
-        references=test_data["output"].to_list(),
-        tokenizer=tokenizer.tokenize, rouge_types=["rouge1", "rouge2", "rougeL"]
+        references=test_data["output"].apply(lambda x: clean_text(x)).to_list(),
+        tokenizer=tokenizer.tokenize,
+        rouge_types=["rouge1", "rouge2", "rougeL"],
     )
     scores = rouge.compute(
         predictions=preds,
         references=test_data["output"].to_list(),
         tokenizer=tokenizer.tokenize,
         rouge_types=["rouge1", "rouge2", "rougeL"],
-        use_aggregator=False
+        use_aggregator=False,
     )
     # {"rouge1": [], "rouge2": [], "rougeL": []}
-    
+
     score_names = scores.keys()
-    
+
     for i in range(len(result_list)):
         a_score = {}
         for sname in score_names:
@@ -106,13 +120,15 @@ def inference(model, tokenizer, test_data: pd.DataFrame):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model", type=str, required=True)
-    parser.add_argument("--format", type=str, required=True, help="[my, koalpaca, kullm]")
+    parser.add_argument(
+        "--format", type=str, required=True, help="[my-v0, my-v1, koalpaca, kullm]"
+    )
     parser.add_argument("--test_file_path", type=str, required=True, help="[.csv]")
     parser.add_argument("--name", type=str, default="")
     args = parser.parse_args()
     print(args)
 
-    now = datetime.now(timezone('Asia/Seoul')).strftime('%m%d%H%M')
+    now = datetime.now(timezone("Asia/Seoul")).strftime("%m%d%H%M")
 
     df = pd.read_csv(args.test_file_path)
 
@@ -124,14 +140,17 @@ if __name__ == "__main__":
     ).to(device=f"cuda", non_blocking=True)
 
     tokenizer = AutoTokenizer.from_pretrained(args.model)
-    
+
     result_json = inference(model, tokenizer, df)
-    
+
     dir_name = "test_result"
-    file_name = args.name + "_" + args.model.strip().split("/")[-1] + "_"+ now +".json"
-    
+    file_name = ""
+    if len(args.name) > 0:
+        file_name += args.name + "_"
+    file_name += args.model.strip().split("/")[-1] + "_" + now + ".json"
+
     os.makedirs(dir_name, exist_ok=True)
-    
+
     path = os.path.join(dir_name, file_name)
 
     with open(path, "w", encoding="utf-8") as f:
