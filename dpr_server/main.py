0588c8dad38707d1c7f70e983771ae9532f7d36e
JLake310 <0310kjy@gmail.com>
2023-07-18 14:19:47 +0900

    [Feat] API v3 추가

--- dpr_server/main.py
+++ dpr_server/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from typing import List
 from pydantic import BaseModel
 from model import DenseRetriever
+from starlette.middleware.cors import CORSMiddleware
 
 
 class Item(BaseModel):
@@ -9,16 +10,42 @@ class Item(BaseModel):
     reviews: List[str]
 
 
+class DBItem(BaseModel):
+    query: str
+    reviews: List[dict]
+
+
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 DPR = DenseRetriever()
 
 
-@app.get("/dpr/concat")
+@app.post("/dpr/concat")
 def retrieve_concat(item: Item):
     return {"review": DPR.run_dpr_concat(item.query, item.reviews)}
 
 
-@app.get("/dpr/split")
+@app.post("/dpr/split")
 def retrieve_split(item: Item):
     return {"review": DPR.run_dpr_split(item.query, item.reviews)}
+
+
+@app.post("/dpr/split_db")
+def retrieve_split_db(item: DBItem):
+    return {"review": DPR.run_dpr_db(item.query, item.reviews)}
+
+
+@app.post("/dpr/split_db_v3")
+def retrieve_split_db_v3(item: DBItem):
+    return {"review": DPR.run_dpr_db_v3(item.query, item.reviews)}
--- dpr_server/model.py
+++ dpr_server/model.py
@@ -1,12 +1,38 @@
 import torch
 import torch.nn.functional as F
-from encoder import HFBertEncoder
+from encoder import HFBertEncoder, BertEncoder
 from typing import Callable, Dict, List, Tuple
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoConfig
+import re
+from kiwipiepy import Kiwi
+import string
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import KMeans
+from collections import defaultdict
+import time
+import numpy as np
+
+
+def filter_corpus(corpus):
+    corpus = [text for text in corpus if text.find("배송") == -1]
+    corpus = [text for text in corpus if text.find("도움") == -1]
+    corpus = [text for text in corpus if len(text) > 10]
+    corpus = [text for text in corpus if len(text) < 200]
+    corpus = [text for text in corpus if text.find("년") == -1]
+    corpus = [text for text in corpus if text.find("ml") == -1]
+    corpus = [text for text in corpus if text.find("날짜") == -1]
+    corpus = [text for text in corpus if text.find("22") == -1]
+    corpus = [text for text in corpus if text.find("23") == -1]
+    corpus = [text for text in corpus if text.find("기한") == -1]
+    corpus = [text for text in corpus if text.find("미리") == -1]
+    corpus = [text for text in corpus if text.find("감사") == -1]
+    return corpus
 
 
 class DenseRetriever:
     def __init__(self):
+        model_name = "klue/bert-base"
+
         self.p_encoder = HFBertEncoder.init_encoder(
             cfg_name="JLake310/bert-p-encoder"
         ).eval()
@@ -14,6 +40,10 @@ class DenseRetriever:
             cfg_name="JLake310/bert-q-encoder"
         ).eval()
         self.tokenizer = AutoTokenizer.from_pretrained("JLake310/bert-p-encoder")
+
+        # self.p_encoder = BertEncoder.from_pretrained(model_name).eval()
+        # self.q_encoder = BertEncoder.from_pretrained(model_name).eval()
+        # self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     def run_dpr_concat(self, query: str, reviews: List[str]) -> str:
         q_seqs_val = self.tokenizer(
@@ -32,6 +62,7 @@ class DenseRetriever:
         return reviews[max_idx]
 
     def run_dpr_split(self, query: str, reviews: List[str]) -> str:
+        start = time.time()
         q_seqs_val = self.tokenizer(
             [query], padding="max_length", truncation=True, return_tensors="pt"
         )
@@ -41,7 +72,7 @@ class DenseRetriever:
         max_idx = 0
         for idx, review in enumerate(reviews):
             r_tokens = self.tokenizer(
-                review,
+                review[:3000],
                 truncation=True,
                 max_length=512,
                 return_overflowing_tokens=True,
@@ -62,5 +93,207 @@ class DenseRetriever:
             if mean_val > max_val:
                 max_val = mean_val
                 max_idx = idx
-
+        end = time.time()
+        print(f"Retrieve 시간 : {end-start:.5f}sec")
         return reviews[max_idx]
+
+    def run_dpr_db(self, query: str, reviews: List[str]) -> str:
+        start = time.time()
+        reviews = [review["context"] for review in reviews]
+        reviews = [re.sub(r"<[^>]+>\s+(?=<)|<[^>]+>", "", text) for text in reviews]
+        reviews = [re.sub(r"[^가-힣a-zA-Z0-9\n\s]", "", text).strip() for text in reviews]
+        kiwi = Kiwi()
+
+        split_texts = []
+        for review in reviews:
+            sents = kiwi.split_into_sents(review)
+            sents = [sent.text for sent in sents]
+            split_texts.append(sents)
+
+        corpus = []
+        for idx, text in enumerate(split_texts):
+            for t in text:
+                corpus.append(str(idx) + " " + t)
+
+        corpus = filter_corpus(corpus)
+
+        clusters = ["평가", "조리"]
+        # data = clusters + filter_corpus(split_texts[text_id])
+        data = clusters + corpus
+        n = len(clusters)
+
+        # Vectorizer
+        remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+        tokenizer_func = lambda x: self.tokenizer.tokenize(
+            x.translate(remove_punct_dict)
+        )
+        vectorizer = TfidfVectorizer(tokenizer=tokenizer_func, ngram_range=(1, 2))
+
+        # Feature vectorize
+        feature_vect = vectorizer.fit_transform(data)
+
+        # 임의 클러스터로 클러스터 진행
+        km_cluster = KMeans(n_clusters=n, max_iter=10000, random_state=0)
+        km_cluster.fit(feature_vect[:n])
+
+        # 임의 클러스터로 새로운 클러스터링 초기화
+        kmeans_new = KMeans(init=km_cluster.cluster_centers_, n_clusters=n)
+        kmeans_new.fit(feature_vect[n:])
+
+        result = [[] for _ in range(n)]
+        for idx, label in enumerate(km_cluster.labels_):
+            result[label].append(clusters[idx])
+
+        for idx, label in enumerate(kmeans_new.labels_):
+            result[label].append(data[idx + n])
+
+        result_dict = {}
+        for r in result:
+            r_dict = defaultdict(list)
+            for text in r[1:]:
+                index = text.split(" ")[0]
+                t = " ".join(text.split(" ")[1:])
+                r_dict[index].append(t)
+            # result_dict[r[0]] = r[1:]
+            result_dict[r[0]] = dict(r_dict)
+
+        filtered_text = [[] for _ in range(len(split_texts))]
+
+        for idx in result_dict["평가"]:
+            filtered_text[int(idx)].extend(result_dict["평가"][idx])
+
+        text_list = [". ".join(text) for text in filtered_text]
+
+        filtered_reviews = []
+        for i in range(10):
+            filtered_reviews.append(" ".join(text_list[i * 20 : i * 20 + 20]))
+
+        end = time.time()
+        print(f"클러스터링 시간 : {end-start:.5f}sec")
+
+        return self.run_dpr_split(query, filtered_reviews)
+
+    def run_dpr_db_v3(self, query: str, reviews: List[str]) -> str:
+        prod_ids = [review["prod_id"] for review in reviews]
+        reviews = [review["context"] for review in reviews]
+        reviews = [re.sub(r"<[^>]+>\s+(?=<)|<[^>]+>", "", text) for text in reviews]
+        reviews = [re.sub(r"[^가-힣a-zA-Z0-9\n\s]", "", text).strip() for text in reviews]
+        kiwi = Kiwi()
+
+        split_texts = []
+        for review in reviews:
+            sents = kiwi.split_into_sents(review)
+            sents = [sent.text for sent in sents]
+            split_texts.append(sents)
+
+        corpus = []
+        for idx, text in enumerate(split_texts):
+            for t in text:
+                corpus.append(str(idx) + " " + t)
+
+        corpus = filter_corpus(corpus)
+
+        clusters = ["평가", "조리"]
+        # data = clusters + filter_corpus(split_texts[text_id])
+        data = clusters + corpus
+        n = len(clusters)
+
+        # Vectorizer
+        remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+        tokenizer_func = lambda x: self.tokenizer.tokenize(
+            x.translate(remove_punct_dict)
+        )
+        vectorizer = TfidfVectorizer(tokenizer=tokenizer_func, ngram_range=(1, 2))
+
+        # Feature vectorize
+        feature_vect = vectorizer.fit_transform(data)
+
+        # 임의 클러스터로 클러스터 진행
+        km_cluster = KMeans(n_clusters=n, max_iter=10000, random_state=0)
+        km_cluster.fit(feature_vect[:n])
+
+        # 임의 클러스터로 새로운 클러스터링 초기화
+        kmeans_new = KMeans(init=km_cluster.cluster_centers_, n_clusters=n)
+        kmeans_new.fit(feature_vect[n:])
+
+        result = [[] for _ in range(n)]
+        for idx, label in enumerate(km_cluster.labels_):
+            result[label].append(clusters[idx])
+
+        for idx, label in enumerate(kmeans_new.labels_):
+            result[label].append(data[idx + n])
+
+        result_dict = {}
+        for r in result:
+            r_dict = defaultdict(list)
+            for text in r[1:]:
+                index = text.split(" ")[0]
+                t = " ".join(text.split(" ")[1:])
+                r_dict[index].append(t)
+            # result_dict[r[0]] = r[1:]
+            result_dict[r[0]] = dict(r_dict)
+
+        filtered_text = [[] for _ in range(len(split_texts))]
+
+        for idx in result_dict["평가"]:
+            filtered_text[int(idx)].extend(result_dict["평가"][idx])
+
+        text_list = [". ".join(text) for text in filtered_text]
+
+        filtered_reviews = []
+        for i in range(10):
+            filtered_reviews.append(" ".join(text_list[i * 20 : i * 20 + 20]))
+
+        q_seqs_val = self.tokenizer(
+            [query], padding="max_length", truncation=True, return_tensors="pt"
+        )
+        q_emb = self.q_encoder(**q_seqs_val)
+
+        sim_score_list = []
+        for review in filtered_reviews:
+            r_tokens = self.tokenizer(
+                review[:3000],
+                truncation=True,
+                max_length=512,
+                return_overflowing_tokens=True,
+                return_offsets_mapping=True,
+                return_token_type_ids=True,
+                padding="max_length",
+                return_tensors="pt",
+            )
+            r_input = {
+                "input_ids": r_tokens["input_ids"],
+                "attention_mask": r_tokens["attention_mask"],
+                "token_type_ids": r_tokens["token_type_ids"],
+            }
+            r_emb = self.p_encoder(**r_input)
+            sim_scores = torch.matmul(r_emb, torch.transpose(q_emb, 0, 1))
+            mean_val = torch.mean(sim_scores)
+            sim_score_list.append(mean_val)
+
+        sim_score_list = np.array(sim_score_list)
+        sort_indices = np.argsort(sim_score_list)
+        recom_list = [
+            {
+                "prod_id": prod_ids[sort_indices[0]],
+                "context": filtered_reviews[sort_indices[0]],
+            },
+            {
+                "prod_id": prod_ids[sort_indices[1]],
+                "context": filtered_reviews[sort_indices[1]],
+            },
+            {
+                "prod_id": prod_ids[sort_indices[2]],
+                "context": filtered_reviews[sort_indices[2]],
+            },
+            {
+                "prod_id": prod_ids[sort_indices[3]],
+                "context": filtered_reviews[sort_indices[3]],
+            },
+            {
+                "prod_id": prod_ids[sort_indices[4]],
+                "context": filtered_reviews[sort_indices[4]],
+            },
+        ]
+
+        return recom_list
