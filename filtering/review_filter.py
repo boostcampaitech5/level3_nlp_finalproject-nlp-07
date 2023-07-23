111908642b187d96784f4ff23ff4eb6a5dc4baa2
lectura7942 <81620001+lectura7942@users.noreply.github.com>
2023-07-23 14:06:17 +0000

    [Feat] 중복 문장 필터링 추가

--- filtering/review_filter.py
+++ filtering/review_filter.py
@@ -1,6 +1,7 @@
 import re
 from tqdm import tqdm
 from typing import List, Union
+from collections import defaultdict
 
 from kiwipiepy import Kiwi
 
@@ -8,13 +9,21 @@ class ReviewFilter:
     def __init__(self):
         self.kiwi = Kiwi()
         self.del_patterns = [
-            "조리", "해동", "요리", '추가','[센중약]불', "넣", "헹", # 레시피
+            "조리", "해동", "요리", '추가','[센중약]불', "넣", "헹", "방법", # 레시피
             # "에어[ ]?[프후]라이[기어]?", "오븐", "전자레인지", "[^ ]*팬", "냄비",
             '작성', '도움', '내돈내산','리뷰','안녕','보답','감사','눌러','좋은 하루', "후기", # 리뷰 끝맺음
-            "(유통)?기한", 
+            "(유통)?기한", "보관",
             "(재)?구매", "(재)?구입",
             "배달", "배송", "로켓", "프레시",'주문',
         ]
+    
+    def split_into_sents(self, text):
+        sents = self.kiwi.split_into_sents(
+                text, 
+                normalize_coda=True, 
+                split_complex=True,
+            )
+        return [se.text for se in sents]
     
     def get_filter_score(self, before_len:int, after_len:int, verbose=False):
         """필터링 비율 반환"""
@@ -48,35 +57,43 @@ class ReviewFilter:
     def filter_length(self, corpus, min_len=10, max_len=200, include_blank = False, verbose=False):
         """[min_len, max_len] 사이에 있는 문장 외 필터링"""
         
-        if verbose:
-            short_tqdm = tqdm(corpus, total=len(corpus), desc="Filtering too short sentences")
-        else:
-            short_tqdm = corpus
-        
-        filtered_corpus = [text for text in short_tqdm
-                        if (
-                            (include_blank and len(text) >= min_len)
-                            or (not include_blank and len(re.sub(" ", "", text)) >= min_len)
-                        )]
-        short_len = len(corpus) - len(filtered_corpus)
-        
-        if verbose:
-            long_tqdm = tqdm(filtered_corpus, total=len(filtered_corpus), desc="Filtering too long sentences")
-        else:
-            long_tqdm = filtered_corpus
-        
-        filtered_corpus = [text for text in long_tqdm 
-                        if(
-                            (include_blank and len(text) <= max_len)
-                            or (not include_blank and len(re.sub(" ", "", text)) <= max_len)
-                        )]
-        long_len = len(corpus) - len(filtered_corpus)
+        filtered_corpus = []
+        short_len = 0
+        long_len = 0
+        for text in corpus:
+            if include_blank: 
+                text_len = len(text)
+            else:
+                text_len = len(re.sub(" ", "", text))
+                
+            if text_len < min_len: 
+                short_len += 1
+            elif text_len > max_len:
+                max_len += 1
+            else:
+                filtered_corpus.append(text)
         
         if verbose:
             print(f"Shorter than {min_len}: {short_len} | Longer than {max_len}: {long_len}")
             
         return filtered_corpus
     
+    def filter_duplicates(self, corpus, verbose=False):
+        """
+        Returns:
+            Tuple(List[str], List[int]): (중복 제거된 corpus, 제거된 문장 인덱스)
+        """
+        unique_corpus = defaultdict(int)
+        remove_idx = []
+        for idx, c in enumerate(corpus):
+            unique_corpus[c] += 1
+            if unique_corpus[c] > 1: remove_idx.append(idx)
+        
+        if verbose:
+            print(f"Removed {len(remove_idx)} duplicate sentences.")
+            
+        return list(unique_corpus.keys()), remove_idx
+            
     
     def filter_text(self, text, verbose=False, join = True)->Union[str, List[str]]:
         """`text`를 문장으로 분리하고, 문장 별 필터링
@@ -86,16 +103,15 @@ class ReviewFilter:
         """
         
         # 문장 분리
-        corpus = []
-        sents = self.kiwi.split_into_sents(
-                text, 
-                normalize_coda=True, 
-                split_complex=True,
-            )
-        corpus = [se.text for se in sents]
+        corpus = self.split_into_sents(text)
+        
+        # 중복 필터링
+        if verbose: print("Filtering duplicates.")
+        filtered_corpus = self.filter_duplicates(corpus, verbose=verbose)[0]
         
         # 길이 필터링
-        filtered_corpus = self.filter_length(corpus, 10, 200, verbose=verbose)
+        if verbose: print("Filtering by length.")
+        filtered_corpus = self.filter_length(filtered_corpus, 10, 200, verbose=verbose)
         
         # 정규식 패턴 필터링
         del_dict = self.check_patterns(self.del_patterns, filtered_corpus, verbose=verbose)
@@ -119,7 +135,8 @@ class ReviewFilter:
     
 if __name__ == "__main__":
     
-    text = "곰곰 소중한 우리 쌀 2022년산 20kg 상 등급 1개 매번 주문하는 쌀입니다 어쩌다 다른 쌀을 주문해서 먹었는데 정말 실망하고 역시 곰곰 우리 쌀이 제일이라는 걸 느꼈네요.. 일단은 가격에 부담스럽지 않아서 주문하는데요 가격만 저렴하고 쌀이 정말 못 먹겠음 안 사잖아요 요건 안 그래요 가격도 착한데 쌀 품질도 좋아요 비싼 쌀은 6만 원대가 넘어가는데 다섯 식구인 저희는 그런 쌀은 못 먹겠더라고요.. ᅲᅲ 근데 요 곰곰 쌀이 정말 맛이 좋고 윤기도 흘러요 밥할 때 구수한 냄새 있잖아요 그게 쌀도 좋아요 좋은 냄새가 나더라고요 밥할 때마다 구수한 냄새가 입맛이 확 돋아나게 합니다. 배송도 꼼꼼하게 잘 오구요 집 앞까지 가져다주시니 정말 땡큐죠 쌀 깨진 것 거의 없고 부서짐이나 가루도 거의 없고요 저렴하게 좋은 쌀 드시고 싶으신 분들 꼭 드셔보세요 한번 빠지면 헤어 나올 수 없는 곰곰입니다"  
+    # text = "떡볶이는 항상 냉동실에 한 개는 있어야 된다는 급당기면 바로 해먹기 편해요 오랜만에 국떡이가 당겨서 구매했어요 600그램 혼자 먹기 딱 좋은 양이에요 떡. 어묵 3장. 소스가 두 개 냉장실에 하루 빼놨더니 해동이 잘 됐어요 물 360미리 넣고 떡. 어묵 3개. 소스 한 번에 다 넣고 10분 정도 끓여주면 옛날 떡볶이가 완성 너무너무 간단해요 밀떡이 아주아주 쫄깃쫄깃 식감이 좋고 어묵 3개도 얇은 게 아니고 조금 도톰해서 더 맛있는 거 같아요 떡볶이 냄새가 죽여줘요 떡볶이 떡이 몇 시간이 지났는데도 딱딱해지지 않고 말랑말랑 식었는데도 맛나요 가격 대비 매우 만족합니다 유통기한도 엄청 길어서 냉동실에 쟁여놔도 좋겠어요 또 구매할게요" 
+    text = "이것은 테스트입니다. 중복 문장 테스트 긴 문장으로. 하하하 중복 문장 테스트. 하하핫. 중복 문장 테스트 긴 문장으로. "
     
     import sys
     sys.path.append("/opt/ml/input/level3_nlp_finalproject-nlp-07")
@@ -128,11 +145,22 @@ if __name__ == "__main__":
     filter = ReviewFilter()
     
     text = clean_text(text, remove_dot=False) # 마침표는 문장 분리에 도움을 주기 때문에 제거하지 않음
+    print("\n=== 전 ===")
+    sents = [
+        s.text for s in filter.kiwi.split_into_sents(
+                text, 
+                normalize_coda=True, 
+                split_complex=True,
+            )]
+    print(sents)
+    print("\n=== 후 ===")
     res = filter.filter_text(text, verbose=True, join=False)
     print(res)
     
     res = filter.filter_text(text, verbose=True)
+    print()
     print(res)
+    print("=== 필터링 비율 ===\n")
     print(filter.get_filter_score(len(text), len(res), verbose=True))
         
         
